f57081980144ec131f442cc74b674e0b7c65d613
fender01ast <rus.k1996@gmail.com>
2025-06-15 21:55:18 +0500

    ver 1.1.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,21 @@ const frameRadios = document.querySelectorAll('input[name="frameType"]');
 
 meshRadios.forEach(radio => {
   radio.addEventListener('change', () => {
-    gridRadios.forEach(gr => gr.checked = false);
+    const gridInput = document.querySelector('input[name="gridType"]:checked');
+    if (!(gridInput && gridInput.value === 'combo')) {
+      gridRadios.forEach(gr => gr.checked = false);
+    }
+    /*gridRadios.forEach(gr => gr.checked = false);*/
   });
 });
 
 gridRadios.forEach(radio => {
   radio.addEventListener('change', () => {
-    meshRadios.forEach(mr => mr.checked = false);
+    const gridInput = document.querySelector('input[name="gridType"]:checked');
+    if (!(gridInput && gridInput.value === 'combo')) {
+      meshRadios.forEach(mr => mr.checked = false);
+    }
+    /*meshRadios.forEach(mr => mr.checked = false);*/
     frameRadios.forEach(fr => fr.checked = false);
   });
 });
@@ -25,6 +33,7 @@ function calculatePrice() {
   document.getElementById("result").style.padding = '8px';
 
   if (isNaN(width) || isNaN(height) || width <= 0 || height <= 0) {
+    document.getElementById("result").style.backgroundColor = "red";
     document.getElementById("result").textContent = "Пожалуйста, введите корректные размеры.";
     return;
   }
@@ -74,7 +83,25 @@ function calculatePrice() {
     framePrice = frameInput?.value === 'inside' ? 0 : -2000;
   }
 
-  const total = Math.round(area * (meshPrice + gridPrice) + framePrice);
 
-  document.getElementById("result").textContent = `Стоимость: ${total.toLocaleString()} тенге.`;
+
+  if (((gridInput) && (gridInput.value === 'combo'))) {
+    if ( !(meshInput && meshInput.value === 'simple') && !(meshInput && meshInput.value === 'plisse') && !(meshInput && meshInput.value === 'custom') ) {
+      const total = Math.round((((area * meshPrice) * 30) / 100) + (area * gridPrice));
+      document.getElementById("result").textContent = `Стоимость: ${total.toLocaleString()} тенге.`;
+      return total;
+    } else {
+      const total = Math.round(area * gridPrice);
+      document.getElementById("result").textContent = `Стоимость: ${total.toLocaleString()} тенге.`;
+      return total;
+    }
+  } else {
+    const total = Math.round(area * (meshPrice + gridPrice) + framePrice);
+    document.getElementById("result").textContent = `Стоимость: ${total.toLocaleString()} тенге.`;
+    return total;
+  }
+
+  /*const total = Math.round(area * (meshPrice + gridPrice) + framePrice);
+
+  document.getElementById("result").textContent = `Стоимость: ${total.toLocaleString()} тенге.`;*/
 }
